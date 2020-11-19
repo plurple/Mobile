7287d3853d54fad18eff10704ab5ffeccdd032af
plurple <mattgrierson@btinternet.com>
2020-11-19 15:28:18 +0000

    set up of the project in android studio

---
+++ app/src/main/java/com/example/dungeonexplorer/MainMenu.kt
@@ -0,0 +1,11 @@
+package com.example.dungeonexplorer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mainMenu)
+    }
+}
